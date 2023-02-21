d48bc9272cd4014e4fb5585d13d5665612792396
Giacomo Nanni <gn.nanni@gmail.com>
2023-02-21 14:38:37 +0100

    feat essay branch

--- src/stores.js
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const selectedMarkdown = writable();
-export const graphData = writable([]);
-export const allLinks = writable([]);
-export const visibleLinks = writable([]);
-export const selectedNode = writable([]);
-export const scrolled = writable(false);
-export const showItemDetail = writable(false);
