57bddbd093accd2560f0e454cc3c038e4c2eddd6
Giacomo Nanni <gn.nanni@gmail.com>
2023-03-08 12:56:13 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const Api = "https://uclab.fh-potsdam.de/refa/api";
-export const Files = ["Sammlungsgeschichte-der-Lipperheideschen"];
-// export const Files = ["test"];
+//export const Files = ["Sammlungsgeschichte-der-Lipperheideschen"];
+export const Files = ["test"];
