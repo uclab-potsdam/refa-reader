a05183d9befbf4e24ee3f3c3803f9d2dbe379a41
Giacomo Nanni <gn.nanni@gmail.com>
2023-03-20 15:41:40 +0100

    fix: essays directory

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,13 +1,12 @@
 export const fetchMarkdownData = async () => {
-    const allPostFiles = import.meta.glob('/src/routes/essays/*.md')
+    const allPostFiles = import.meta.glob('/src/routes/texts/*.md')
     const iterablePostFiles = Object.entries(allPostFiles)
 
     const allPosts = await Promise.all(
         iterablePostFiles.map(async ([path, resolver]) => {
             const resolvedPost = await resolver();
             const { html } = resolvedPost.default.render();
-            const postPath = path.slice(11, -3)
-
+            const postPath = "/essays" + path.slice(17, -3)
             return {
                 meta: resolvedPost.metadata,
                 path: postPath,
