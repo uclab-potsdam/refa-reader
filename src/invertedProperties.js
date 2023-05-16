347cac4de3b8478bd96c48975803065b0a8102b7
Giacomo Nanni <gn.nanni@gmail.com>
2023-05-16 14:56:05 +0200

    fix loading

--- src/invertedProperties.js
+++ src/invertedProperties.js
@@ -1,8 +1,11 @@
 export const invertedProperties = {
+    "is shown by": "shows visual item",
+    "is composed of": "forms part of",
+
     "creator": "created",
     "has type": "is type of",
     "forms part of": "consists of",
-    "shows visual item": "shows features of",
+    "shows visual item": "is shown by",
     "shows features of": "features are also found on",
     "refers to": "shows features of",
     "broader": "narrower",
