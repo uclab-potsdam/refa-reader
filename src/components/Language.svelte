30154b62b6e5707cbf07e1836b414ecba834ad97
Giacomo Nanni <gn.nanni@gmail.com>
2023-07-26 12:09:15 +0200

    feat store language in localstorage

--- src/components/Language.svelte
+++ src/components/Language.svelte
@@ -1,14 +1,37 @@
 <script>
+	import { onMount } from 'svelte';
+
 	export let lang;
 
 	function switchLanguage(newLang) {
 		lang = newLang;
+		localStorage.setItem('selectedLanguage', newLang);
 	}
+
+	onMount(() => {
+		const storedLanguage = localStorage.getItem('selectedLanguage');
+		if (storedLanguage) {
+			lang = storedLanguage;
+		}
+	});
 </script>
 
 <div>
-	<span on:click={() => switchLanguage('en')} class:selected={lang === 'en'} disabled={lang === 'en'}>EN</span> /
-	<span on:click={() => switchLanguage('de')} class:selected={lang === 'de'} disabled={lang === 'de'}> DE</span>
+	<span
+		on:click={() => switchLanguage('en')}
+		on:keydown={() => switchLanguage('en')}
+		class:selected={lang === 'en'}
+		disabled={lang === 'en'}>EN</span
+	>
+	/
+	<span
+		on:click={() => switchLanguage('de')}
+		on:keydown={() => switchLanguage('de')}
+		class:selected={lang === 'de'}
+		disabled={lang === 'de'}
+	>
+		DE</span
+	>
 </div>
 
 <style>
@@ -16,10 +39,10 @@
 		position: absolute;
 		top: 10px;
 		right: 10px;
-        cursor: pointer;
+		cursor: pointer;
 	}
 
 	.selected {
-        color: blue;
+		color: blue;
 	}
 </style>
