057c986151de43f5d4a9198267814aef53272a6d
Giacomo Nanni <gn.nanni@gmail.com>
2023-08-02 09:39:37 +0200

    fix svelte config ⚙️

---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	preprocess: [
+		mdsvex(mdsvexConfig)
+	],
+	kit: {
+		adapter: adapter(),
+		alias: {
+			'@components': 'src/components',
+			'@stores': 'src/stores.js',
+			'@utils': 'src/utils.js',
+		}
+	}
+};
+
+export default config;
