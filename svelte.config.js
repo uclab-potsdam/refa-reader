ec55786c1c213cc34336c14b5821504394d119f5
Giacomo Nanni <gn.nanni@gmail.com>
2023-10-11 17:13:41 +0200

    fix PUBLIC_BASE_PATH

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			precompress:true
 		}),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
+			base: dev ? '' : process.env.PUBLIC_BASE_PATH,
 		},
 		prerender: {
 			crawl: true,
