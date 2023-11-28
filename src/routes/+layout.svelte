780495a52d090ed69d665ccc98dec47e68dad9d2
Giacomo Nanni <gn.nanni@gmail.com>
2023-11-28 11:28:25 +0100

    fix path title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,15 +6,15 @@
 </script>
 
 <svelte:head>
-	<title>{essayData?.meta?.title}</title>
+	<title>{essayData?.meta?.title || config.title}</title>
 	<meta name="description" content={essayData?.meta?.description || config.descriptionSeo} />
 	<meta property="og:url" content={$page.url.origin} />
-	<meta property="og:title" content={essayData?.meta?.title} />
+	<meta property="og:title" content={essayData?.meta?.title || config.title} />
 	<meta property="og:description" content={essayData?.meta?.description || config.descriptionSeo} />
 	<meta property="og:image" content={essayData?.meta?.cover|| config.imageSeo} />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta property="twitter:url" content={$page.url.origin} />
-	<meta name="twitter:title" content="Restaging Fashion" />
+	<meta name="twitter:title" content={essayData?.meta?.title || config.title} />
 	<meta name="twitter:description" content={essayData?.meta?.description || config.descriptionSeo} />
 	<meta name="twitter:image" content={essayData?.meta?.cover|| config.imageSeo} />
 </svelte:head>
